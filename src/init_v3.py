557899a60367b452a00db9a89fe460d86455c5ee
Contramundum <contramundum53@outlook.com>
2022-05-16 19:43:39 +0900

    add report tests and nan/inf/Pruned/Failed tests

---
+++ src/init_v3.py
@@ -0,0 +1,55 @@
+import optuna
+# import pymssql
+
+from objective import *
+
+
+# def create_mssql_database() -> None:
+#     conn = pymssql.connect("mssql", "sa", "optuna-test-5ZYB")
+#     conn.autocommit(True)
+#     cursor = conn.cursor()
+#     cursor.execute("CREATE DATABASE optuna")
+
+
+# create_mssql_database()
+
+
+print("mysql")
+study1 = optuna.create_study(study_name="test", storage="mysql+pymysql://root:root@mysql:3306/optuna", sampler=optuna.samplers.RandomSampler(0))
+try:
+    study1.optimize(objective_with_inf_nan, n_trials=10)
+except BaseException:
+    pass
+print("postgresql")
+study2 = optuna.create_study(study_name="test", storage="postgresql+psycopg2://root:root@postgresql/optuna", sampler=optuna.samplers.RandomSampler(0))
+try:
+    study2.optimize(objective_with_inf_nan, n_trials=10)
+except BaseException:
+    pass
+
+print("sqlite")
+study3 = optuna.create_study(study_name="test", storage="sqlite:///data/sample.db", sampler=optuna.samplers.RandomSampler(0))
+try:
+    study3.optimize(objective_with_inf_nan, n_trials=10)
+except BaseException:
+    pass
+print("mssql")
+study4 = optuna.create_study(study_name="test", storage="mssql+pymssql://sa:optuna-test-5ZYB@mssql/optuna?charset=utf8", sampler=optuna.samplers.RandomSampler(0))
+try:
+    study4.optimize(objective_with_inf_nan, n_trials=10)
+except BaseException:
+    pass
+print("redis")
+study5 = optuna.create_study(study_name="test", storage="redis://redis:6379", sampler=optuna.samplers.RandomSampler(0))
+try:
+    study5.optimize(objective_with_inf_nan, n_trials=10)
+except BaseException:
+    pass
+
+print(study1)
+print(study2)
+print(study3)
+# print(study4)
+print(study5)
+
+print("done")
--- src/objective.py
+++ src/objective.py
@@ -1,4 +1,5 @@
 import optuna
+import optuna.exceptions
 
 
 def objective(trial: optuna.Trial) -> float:
@@ -9,4 +10,81 @@ def objective(trial: optuna.Trial) -> float:
     y2 = trial.suggest_int("y2", 1, 10, log=True)
     y3 = trial.suggest_int("y3", 0, 10, step=2)
     c1 = trial.suggest_categorical("c1", [1, 10, 100])
-    return x1**y1 + x2**y2 + x3**y3 + c1
+    c2 = trial.suggest_categorical("c2", ['value', 'pruned'])
+    
+
+    print("trial_id:", trial._trial_id)
+
+    if trial._trial_id == 9:
+        raise Exception("fail")
+
+    if c2 == 'value':
+        val = x1**y1 + x2**y2 + x3**y3 + c1
+    elif c2 == 'pruned':
+        raise optuna.exceptions.TrialPruned()
+    
+    trial.report(val, step=1)
+    return val
+
+
+def objective_with_inf(trial: optuna.Trial) -> float:
+    x1 = trial.suggest_float("x1", 0, 10)
+    x2 = trial.suggest_float("x2", 1, 10, log=True)
+    x3 = trial.suggest_float("x3", 0, 10, step=2.0)
+    y1 = trial.suggest_int("y1", 0, 10)
+    y2 = trial.suggest_int("y2", 1, 10, log=True)
+    y3 = trial.suggest_int("y3", 0, 10, step=2)
+    c1 = trial.suggest_categorical("c1", [1, 10, 100])
+    c2 = trial.suggest_categorical("c2", ['value', 'pos_inf', 'neg_inf', 'pruned'])
+    
+    print("trial_id:", trial._trial_id)
+
+    if trial._trial_id == 9:
+        raise Exception("fail")
+    
+
+    if c2 == 'value':
+        val = x1**y1 + x2**y2 + x3**y3 + c1
+    elif c2 == 'pos_inf':
+        val = float('inf')
+    elif c2 == 'neg_inf':
+        val = float('-inf')
+    elif c2 == 'pruned':
+        # trial.report(float('nan'), step=1)
+        raise optuna.exceptions.TrialPruned()
+    trial.report(val, step=1)
+    return val
+
+
+def objective_with_inf_nan(trial: optuna.Trial) -> float:
+    x1 = trial.suggest_float("x1", 0, 10)
+    x2 = trial.suggest_float("x2", 1, 10, log=True)
+    x3 = trial.suggest_float("x3", 0, 10, step=2.0)
+    y1 = trial.suggest_int("y1", 0, 10)
+    y2 = trial.suggest_int("y2", 1, 10, log=True)
+    y3 = trial.suggest_int("y3", 0, 10, step=2)
+    c1 = trial.suggest_categorical("c1", [1, 10, 100])
+    c2 = trial.suggest_categorical("c2", ['value', 'pos_inf', 'neg_inf', 'nan', 'pruned'])
+    
+
+
+    print("trial_id:", trial._trial_id)
+    if trial._trial_id == 9:
+        raise Exception("fail")
+
+
+    if c2 == 'value':
+        val = x1**y1 + x2**y2 + x3**y3 + c1
+    elif c2 == 'pos_inf':
+        val = float('inf')
+    elif c2 == 'neg_inf':
+        val = float('-inf')
+    elif c2 == 'nan':
+        val = float('nan')
+    elif c2 == 'pruned':
+        # trial.report(float('nan'), step=1)
+        raise optuna.exceptions.TrialPruned()
+
+    
+    trial.report(val, step=1)
+    return val
