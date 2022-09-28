dfcb1b9fb2d474c8c1ef3738f7d06549548cc0d8
Contramundum <contramundum53@outlook.com>
2022-09-28 09:45:30 +0900

    add patch

--- src/objective.py
+++ src/objective.py
@@ -9,4 +9,8 @@ def objective(trial: optuna.Trial) -> float:
     y2 = trial.suggest_int("y2", 1, 10, log=True)
     y3 = trial.suggest_int("y3", 0, 10, step=2)
     c1 = trial.suggest_categorical("c1", [1, 10, 100])
+    trial.report(0, 1)
+    trial.report(float("inf"), 2)
+    trial.report(float("-inf"), 3)
+    trial.report(float("nan"), 4)
     return x1**y1 + x2**y2 + x3**y3 + c1
