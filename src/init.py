bd672f0a80979463758bf9ea402dffd7f7d7661e
Contramundum <contramundum53@outlook.com>
2022-09-28 09:53:48 +0900

    change objective

--- src/init.py
+++ src/init.py
@@ -1,7 +1,7 @@
 import optuna
 import pymssql
 
-from objective import objective
+from objective import objective_factory
 from optuna.samplers import RandomSampler
 
 
@@ -17,19 +17,19 @@ create_mssql_database()
 
 print("mysql")
 study1 = optuna.create_study(study_name="test", storage="mysql+pymysql://root:root@mysql:3306/optuna", sampler=RandomSampler())
-study1.optimize(objective, n_trials=10)
+study1.optimize(objective_factory(allow_inf=False, allow_nan=False), n_trials=10)
 print("postgresql")
 study2 = optuna.create_study(study_name="test", storage="postgresql+psycopg2://root:root@postgresql/optuna", sampler=RandomSampler())
-study2.optimize(objective, n_trials=10)
+study2.optimize(objective_factory(allow_inf=True, allow_nan=True), n_trials=10)
 print("sqlite")
 study3 = optuna.create_study(study_name="test", storage="sqlite:///data/sample.db", sampler=RandomSampler())
-study3.optimize(objective, n_trials=10)
+study3.optimize(objective_factory(allow_inf=True, allow_nan=True), n_trials=10)
 print("mssql")
 study4 = optuna.create_study(study_name="test", storage="mssql+pymssql://sa:optuna-test-5ZYB@mssql/optuna?charset=utf8", sampler=RandomSampler())
-study4.optimize(objective, n_trials=10)
+study4.optimize(objective_factory(allow_inf=True, allow_nan=True), n_trials=10)
 print("redis")
 study5 = optuna.create_study(study_name="test", storage="redis://redis:6379", sampler=RandomSampler())
-study5.optimize(objective, n_trials=10)
+study5.optimize(objective_factory(allow_inf=True, allow_nan=True), n_trials=10)
 
 print(study1)
 print(study2)
--- src/objective.py
+++ src/objective.py
@@ -1,16 +1,23 @@
 import optuna
 
 
-def objective(trial: optuna.Trial) -> float:
-    x1 = trial.suggest_float("x1", 0, 10)
-    x2 = trial.suggest_float("x2", 1, 10, log=True)
-    x3 = trial.suggest_float("x3", 0, 10, step=2.0)
-    y1 = trial.suggest_int("y1", 0, 10)
-    y2 = trial.suggest_int("y2", 1, 10, log=True)
-    y3 = trial.suggest_int("y3", 0, 10, step=2)
-    c1 = trial.suggest_categorical("c1", [1, 10, 100])
-    trial.report(0, 1)
-    trial.report(float("inf"), 2)
-    trial.report(float("-inf"), 3)
-    trial.report(float("nan"), 4)
-    return x1**y1 + x2**y2 + x3**y3 + c1
+def objective_factory(allow_inf: bool, allow_nan: bool):
+    def objective(trial: optuna.Trial) -> float:
+        x1 = trial.suggest_float("x1", 0, 10)
+        x2 = trial.suggest_float("x2", 1, 10, log=True)
+        x3 = trial.suggest_float("x3", 0, 10, step=2.0)
+        y1 = trial.suggest_int("y1", 0, 10)
+        y2 = trial.suggest_int("y2", 1, 10, log=True)
+        y3 = trial.suggest_int("y3", 0, 10, step=2)
+        c1 = trial.suggest_categorical("c1", [1, float("inf"), float("-inf")])
+        trial.report(0, 1)
+        if allow_inf:
+            trial.report(float("inf"), 2)
+            trial.report(float("-inf"), 3)
+        if allow_nan:
+            trial.report(float("nan"), 4)
+        
+        if allow_inf:
+            return c1
+        else:
+            return x1**y1 + x2**y2 + x3**y3
